f0374d2f3e804eb17c7edd0d37bcbb4be411f9cd
Nza6920 <1484663282@qq.com>
2020-02-15 20:31:28 +0800

    city choose

---
+++ pages/changecity.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="page-changeCity">
+    <el-row>
+      <el-col :span="24">
+        <i-select />
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <hot />
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <categroy />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  import iSelect from '../components/changeCity/iselect'
+  import hot from '../components/changeCity/hot'
+  import categroy from '../components/changeCity/categroy'
+
+  export default {
+    components: {
+      iSelect,
+      hot,
+      categroy
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
